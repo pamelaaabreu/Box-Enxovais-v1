1019dc34d69a5936c4fd8632db6df23c4e9c1c42
Pamela <vitoria201547@gmail.com>
2025-07-16 19:50:16 -0300

    Ajustando navbar

--- frontend/src/app/minha-conta/cadastro/cadastro.component.ts
+++ frontend/src/app/minha-conta/cadastro/cadastro.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
 
+  name = new FormControl('');
 }
